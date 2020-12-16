fe4cc6b3686521fc59c6611c9a8e27852d9dbf46
nakangpai <66093080+nakangpai@users.noreply.github.com>
2020-12-16 14:34:08 +0900

    [김성훈] vo주석 위치변경 ,코드 줄맞춤 통일 manager xml 쿼리추가, manager controller (작성중)

--- lms/src/main/java/gd/fintech/lms/manager/controller/ManagerNoticeController.java
+++ lms/src/main/java/gd/fintech/lms/manager/controller/ManagerNoticeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import gd.fintech.lms.manager.service.ManagerNoticeService;
@@ -17,10 +19,16 @@ public class ManagerNoticeController {
 	@Autowired ManagerNoticeService managerNoticeService;
 	
 	//공지사항 리스트 
-	@GetMapping ("")
+	@GetMapping ("/manager/LmsNoticeList/{lmsNoticeNo}/{currentPage}")
 	public String LmsNotice(Model model, @RequestParam(value = "lmsNoticeNo") int lmsNoticeNo, @RequestParam(value = "currentPage") int currentPage) {
+		
+		//한 페이지 출력 공지사항 개수 
 		int rowPerPage = 10;
+		
+		//last 페이지
 		int lastPage = 0; 
+		
+		//공지사항 total Count
 		int totalCount = managerNoticeService.getLmsNoticeCount(lmsNoticeNo);
 				
 		if(totalCount % rowPerPage == 0) {
@@ -35,8 +43,19 @@ public class ManagerNoticeController {
 		model.addAttribute("lastpage", lastPage);
 		model.addAttribute("lmsnoticeList", LmsnoticeList);
 	
-		return "";
+		return "manager/LmsNoticeList";
 	}
 	
+	//공지사항 작성 폼 
+	@GetMapping ("/manager/addLmsNotice/{LmsNotice}")
+	public String addLmsNotice(@PathVariable(value="LmsNotice")int lmsNoticeNo) {
+		return "mananger/addLmsNotice";
+	}
+	
+	//공지사항 작성 액션 
+	@PostMapping("/manager/addLmsNotice/{LmsNotice}")
+	public String addLmsNotice(LmsNotice lmsNotice) {
+		return "";
+	}
 	
 }
--- lms/src/main/java/gd/fintech/lms/vo/LmsNotice.java
+++ lms/src/main/java/gd/fintech/lms/vo/LmsNotice.java
@@ -4,12 +4,28 @@ import lombok.Data;
 
 @Data
 public class LmsNotice {
-	private int lmsNoticeNo; // lms공지사항 번호 
-	private String accountId; // 작성자 id
-	private String lmsNoticeWriter; // 작성자 닉네임? 이름?
-	private String lmsNoticeTitle; // 공지사항 제목
-	private String lmsNoticeContent; // 공지사항 상세내용 
-	private String lmsNoticeCreatedate; // 작성일
-	private String lmsNoticeUpdatedate; // 공지사항 수정일
-	private int lmsNoticeCount; // 조회수 
+	
+	//lms공지사항 번호 
+	private int lmsNoticeNo; 
+	
+	//작성자 id
+	private String accountId; 
+	
+	//작성자 닉네임? 이름?
+	private String lmsNoticeWriter; 
+	
+	//공지사항 제목
+	private String lmsNoticeTitle; 
+	
+	//공지사항 상세내용
+	private String lmsNoticeContent;  
+	
+	//작성일
+	private String lmsNoticeCreatedate; 
+	
+	//공지사항 수정일
+	private String lmsNoticeUpdatedate; 
+	
+	//조회수
+	private int lmsNoticeCount;  
 }
