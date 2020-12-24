e2d747dff051356b99c9c53c463e6abbd2e7281f
byoungyoon <70301218+byoungyoon@users.noreply.github.com>
2020-12-24 18:28:49 +0900

    [이병윤]login페이지 접근 제한 필터 적용
    1.로그인이 성공해서 필터가 적용 되었을 때 로그인페이지나 회원가입 페이지로 가는것을 막음(세션 값을 확인하여 만약 세션이
    존재한다면 각각의 index사이트로 redirect)

---
+++ lms/src/main/java/gd/fintech/lms/LmsIndexFilter.java
@@ -0,0 +1,42 @@
+package gd.fintech.lms;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(urlPatterns = {"/","/teacherLogin", "/teacherSignup", "/studentLogin", "/studentSignup", "/managerLogin", "/managerSignup", "/adminLogin", "/adminSignup"})
+public class LmsIndexFilter implements Filter {
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpSession session = ((HttpServletRequest)request).getSession();
+		String cPath = ((HttpServletRequest) request).getContextPath();
+		if(session.getAttribute("teacherId") != null) {
+			((HttpServletResponse)response).sendRedirect(cPath + "/teacher");
+			return;
+		} 
+		if(session.getAttribute("studentId") != null) {
+			((HttpServletResponse)response).sendRedirect(cPath + "/student");
+			return;
+		} 
+		if(session.getAttribute("managerId") != null) {
+			((HttpServletResponse)response).sendRedirect(cPath + "/manager");
+			return;
+		} 
+		if(session.getAttribute("adminId") != null) {
+			((HttpServletResponse)response).sendRedirect(cPath + "/admin");
+			return;
+		} 
+		
+		chain.doFilter(request, response);
+	}
+
+}
+
